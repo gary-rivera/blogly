36f76eaa2dc119a9b358901b5d4801f1a592223f
Gary Rivera <a.gary.rivera@gmail.com>
2021-03-11 15:47:33 -0800

    part one done. about done with part two

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 
@@ -14,6 +14,8 @@ toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+#### Users routes ###
 
 
 @app.route('/')
@@ -61,6 +63,7 @@ def user_details(user_id):
     user_data = User.query.get_or_404(user_id)
     return render_template('userDetails.html', user=user_data)
 
+
 @app.route('/users/<int:user_id>/edit')
 def show_edit_user_form(user_id):
     """Direct to the user edit form page"""
@@ -90,4 +93,55 @@ def delete_user(user_id):
     db.session.commit()
 
     return redirect("/users")
-# # cross relationship between tables
+
+
+### Posts routes ###
+
+@app.route('/users/<int:user_id>/posts/new')
+def add_post_form(user_id):
+    """render page for user to add a post"""
+    user_data = User.query.get_or_404(user_id)
+
+    return render_template('addPost.html', user=user_data)
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=['POST'])
+def add_post(user_id):
+    """ handle new post form redirect to user details """
+    user_data = User.query.get_or_404(user_id)
+    post = Post(
+        title=request.form['title'],
+        content=request.form['content'],
+        user=user_data)
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(f"/users/{user_id}")
+
+
+@app.route('/users/<int:user_id>/posts/<int:post_id>')
+def show_post(user_id, post_id):
+    """ generate a page with the corresponding users post """
+    post = Post.query.get_or_404(post_id)
+
+    return render_template('postDetails.html', post=post)
+
+
+"""
+GET /users/[user-id]/posts/new
+Show form to add a post for that user.
+POST /users/[user-id]/posts/new
+Handle add form; add post and redirect to the user detail page.
+GET /posts/[post-id]
+Show a post.
+
+Show buttons to edit and delete the post.
+
+GET /posts/[post-id]/edit
+Show form to edit a post, and to cancel (back to user page).
+POST /posts/[post-id]/edit
+Handle editing of a post. Redirect back to the post view.
+POST /posts/[post-id]/delete
+Delete the post.
+"""
