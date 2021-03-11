acaf868457e59e91d4f3c4e5186ade32ea064c05
Gary Rivera <a.gary.rivera@gmail.com>
2021-03-10 17:26:53 -0800

    almost done with first step :)

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from models import db, connect_db, User
 
 app = Flask(__name__)
 
@@ -14,3 +14,52 @@ toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+
+@app.route('/')
+def root():
+    """ redirect to users page"""
+
+    return redirect('/users')
+
+
+@app.route('/users')
+def list_users():
+    """ list current users in db"""
+
+    users = User.query.order_by(User.last_name).all()
+    return render_template('index.html', users=users)
+
+
+@app.route('/users/new')
+def show_new_user_form():
+    """ render the form to create new user and post """
+
+    return render_template('newUser.html')
+
+
+@app.route('/users/new', methods=['POST'])
+def create_user():
+    """ forward form input to generate a new user """
+
+    # add logic if url empty
+    user_data = User(
+        first_name=request.form['first_name'],
+        last_name=request.form['last_name'],
+        image_url=request.form['image_url'] or None)
+
+    db.session.add(user_data)
+    db.session.commit()
+
+    return redirect('/users')
+
+
+@app.route('/users/<int:user_id>')
+def user_details(user_id):
+    """rendering user details"""
+
+    user_data = User.query.get_or_404(user_id)
+    return render_template('userDetails.html', user=user_data)
+
+
+# # cross relationship between tables
--- models.py
+++ models.py
@@ -1,32 +1,38 @@
 from flask_sqlalchemy import SQLAlchemy
+
 db = SQLAlchemy()
 
 
-class User(db.model):
+class User(db.Model):
+    """user db info """
     __tablename__ = 'users'
     id = db.Column(db.Integer,
                    primary_key=True,
                    autoincrement=True)
     first_name = db.Column(
         db.String(50),
-        nullable=False,
-        unique=True 
-    )
+        nullable=False)
     last_name = db.Column(
         db.String(50),
-        nullable=False,
-        unique=True )
+        nullable=False)
     image_url = db.Column(
         db.Text,
         nullable=False,
-        default='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
-    )
+        default='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png')
 
-    __tablename__ = 'tablename2'
-    __tablename__ = 'tablename3'
+    # __tablename__ = 'tablename3'
 
     def __repr__(self):
         """Show info about user."""
 
         u = self
-        return f"<USer {u.id} {u.name} {u.species} {u.hunger}>"
+
+        return f"<User {u.id} {u.first_name} {u.first_name}{u.last_name}>"
+
+    # function to generate user name
+
+
+def connect_db(app):
+    """ connect to db """
+    db.app = app
+    db.init_app(app)
